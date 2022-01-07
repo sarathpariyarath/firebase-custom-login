05494afbdf95daf01fa62e47b5b2cd4de013d25b
sarathpariyarath <sarath.pariyarath@gmail.com>
2022-01-07 17:53:42 +0530

    Initial Commit

---
+++ firebase-authentication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase-authentication
+//
+//  Created by Sarath P on 07/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
